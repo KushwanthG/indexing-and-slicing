0b28609a748a5089ceb8ddb263e881395c1b686b
Kushwanth Gondi <36468390+KushwanthG@users.noreply.github.com>
2020-04-07 16:49:05 +0530

    Add files via upload

---
+++ indexgrabbing.py
@@ -0,0 +1,20 @@
+mystring="hello world"
+##hello world=012345678910,
+## hello world= from backside -1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-11
+print(mystring[0]+mystring[5]+mystring[-6])
+print(mystring)
+print(mystring[2:])
+print(mystring[:])
+print(mystring[:3])
+print(mystring[-6:])
+print(mystring[::-4])
+print(mystring)
+print(mystring[3:7])
+print(mystring[::])
+print(mystring[::4])
+print(mystring[::-4])
+print(mystring)
+print(mystring[2:7:2])
+
+
+
